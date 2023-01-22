db8ff945e668e45e7b75443ab60d31561c0205ba
Abdulrahman Amir Mohamed <a.amer_17@yahoo.com>
2023-01-22 10:21:02 +0200

    Angular

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './interfaces/IPagination';
+import { IProduct } from './interfaces/IProduct';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Amir';
+  Products: IProduct[];
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get("http://localhost:7001/api/products?pageSize=20").subscribe((response: IPagination) => {
+      this.Products = response.data;
+    }, error => { console.log(Error) }
+    )
+  }
 }
---
+++ Client/src/app/interfaces/IProduct.ts
@@ -0,0 +1,9 @@
+export interface IProduct {
+    id: number;
+    name: string;
+    photoUrl: string;
+    description: string;
+    price: number;
+    productType: string;
+    productBrand: string;
+}
